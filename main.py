9c4e19198c6f71d3d133966af9ea012873ca2947
thisnugroho <this.nugroho@gmail.com>
2019-08-02 14:42:46 +0800

    ver 0.1 done

--- main.py
+++ main.py
@@ -2,21 +2,31 @@
 
 import pyautogui, sys, os
 import time
+from pynput.keyboard import *
 
 # to do next 
 # try to put value on x,y and x1, y1
 # try the app
 
 #------------------------------------
+# Setup
+# =================================
+# Position 1
+x = 588
+y = 331
+# if there is any Position
+x1 = 793
+y1 = 338
+# Delay
+delay = 0.5
+# Set Shortcut
+p = Key.f1 # Pause
+r = Key.f2 # Resume
+s = Key.esc # Exit / Stop
+# ===============================
 
-# setup
-# x = some number
-# y = some number
-# if there is any place
-# x1 = some number
-# y1 = some number
-# delay = some number
-# repeat = ? 
+pause = False
+running = True
 
 def cekPosisi():
     print('Press Ctrl-C to quit.')
@@ -30,7 +40,21 @@ def cekPosisi():
             print('\n')
 
 def auto(x, y):
-        pyautogui.click(x, y)
+        pyautogui.click(x = x, y = y)
+
+def on_press(key):
+        global running, pause
+
+        if key == r:
+                pause = False
+                print('Resumed')
+        elif key == p:
+                pause = True
+                print('Paused')
+        elif key == s:
+                running = False
+                exit(0)
+
 
 def main():
     print('Ice Auto Clicker Tools')
@@ -45,7 +69,19 @@ def main():
         cekPosisi()
 
     elif opsi == '2':
-            pass
-
+            lis = Listener(on_press=on_press)
+            lis.start()
+            while running:
+                    if not pause:
+                            auto(x, y)
+                            auto(x1, y1)
+                            pyautogui.PAUSE = delay
+            lis.stop()
+            print('See Ya !')
+    else:
+            print('Sorry,Wrong Input')
 if __name__ == '__main__':
-    main()
+        try:
+                main()
+        except KeyboardInterrupt:
+                print('\nBye')
